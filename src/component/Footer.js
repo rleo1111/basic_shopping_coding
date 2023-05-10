df0e032b5ad4b497ee4e0ad501708d1ac5b79109
kwon <kf3907@naver.com>
2023-05-10 22:42:04 +0800

    1st Footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,3 +1,30 @@
-function Footer() {}
+import logo from "../assets/logo.svg";
+import circle from "../assets/Ellipse.png";
+import "../css/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="first-container">
+        <div className="footer-item left">Community</div>
+        <div className="footer-item left">Mobile app</div>
+        <div className="footer-item left">Company</div>
+        <img className="footer-logo" src={logo} alt="logo" />
+        <div className="footer-item right">Help desk</div>
+        <div className="footer-item right">Blog</div>
+        <div className="footer-item right">Resources</div>
+      </div>
+      <div>
+        <img className="footer-circle" src={circle} alt="circle" />
+        <img className="footer-circle" src={circle} alt="circle" />
+        <img className="footer-circle" src={circle} alt="circle" />
+        <img className="footer-circle" src={circle} alt="circle" />
+      </div>
+      <div className="footer-message">
+        © Photo, Inc. 2019. We love our users!
+      </div>
+    </div>
+  );
+}
 
 export default Footer;
